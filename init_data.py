dfd91131cfa151374e0efdc78191b7b8c1d3b627
tobbaijas <topi.halme@helsinki.fi>
2020-02-19 15:56:45 +0200

    merged airport data

--- init_data.py
+++ init_data.py
@@ -10,10 +10,28 @@ routes.insert(2, "Count", routes.groupby('Route')['Route'].transform('count'), T
 routes = routes.drop_duplicates()
 routes.sort_values(by=['Route'])
 
+### Merge with airport info
+
+airport_cols = ('airport_id', 'airport_name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst',
+               "Tz database time zone",'Type','source')
+airports = pd.read_csv('airports.txt',  names = airport_cols,header = None)
+
+routes = routes.merge(airports, left_on = "from", right_on = "iata") \
+.merge(airports, left_on='to', right_on='iata', suffixes=('_source', '_dest'))
+
+### Create graph
+
 G = nx.convert_matrix.from_pandas_edgelist(routes, 2, 4, 'Count')
 
 print(G.number_of_edges())
 print(G.number_of_nodes())
+
+# Get connected components
+
+graphs = list(nx.connected_component_subgraphs(G))
+G1 = graphs[0] ## Main component
+
+
 
 # find where airplanes are born and which are the absorbing states
 a = routes[2].drop_duplicates()
